acfcc462cec124abbeca9a2fbec5be180de6c268
Jorge <jorge.rodriguez.veiga@usc.es>
2019-07-08 12:41:30 +0200

    Add get initial solution function.

--- firedecomp/fix_work/utils.py
+++ firedecomp/fix_work/utils.py
@@ -178,7 +178,7 @@ def get_minimum_resources(data):
 
 
 # format_fix_work --------------------------------------------------------------
-def format_fix_work(message):
+def format_fix_work(*message):
     return " | ".join([format_fix_work_element(e) for e in message])
 # --------------------------------------------------------------------------- #
 
@@ -203,4 +203,157 @@ def format_fix_work_element(element):
                 return " "*(10-len(str_num)) + str_num
     else:
         return log_s_format.format(str(element))
+# --------------------------------------------------------------------------- #
+
+
+# get_initial_sol -------------------------------------------------------------
+def get_initial_sol(problem_data):
+    """Get initial solution."""
+    data = problem_data.data
+
+    info = get_start_info(
+        data=data,
+        start_dict={
+            (i, t): 1 if t == 1 else 0
+            for i, t in problem_data.resources_wildfire.get_names()
+        }
+    )
+
+    sum_work_periods = {
+        t: {
+            i: sum([info['work'][i, t] * data.PR[i, t]
+                    for t in data.T_int(p_max=t)
+                    if t <= data.WP[i] - data.CUP[i]])
+
+            for i in data.I
+        }
+        for t in data.T}
+
+    max_performance = dict()
+    max_performance_resources = dict()
+    for p in problem_data.wildfire:
+        p_key = p.get_index()
+        max_performance_resources[p_key] = []
+        max_performance[p_key] = 0
+        for gp in p.__group_period__:
+            group_resources = {
+                r.get_index(): sum_work_periods[p_key][r.get_index()]
+                for r in gp.group if sum_work_periods[p_key][r.get_index()] > 0
+            }
+            max_performance_resources[p_key] += sorted(
+                group_resources, reverse=True, key=group_resources.get
+            )[:gp.max_res_groups]
+
+        max_performance[p_key] += sum([
+            sum_work_periods[p_key][res]
+            for res in max_performance_resources[p_key]]
+        )
+
+    con_p = None
+    for p in problem_data.wildfire:
+        if p.perimeter <= max_performance[p.get_index()]:
+            con_p = p.get_index()
+            break
+
+    if con_p is not None:
+        z = {
+            r: r in max_performance_resources[con_p]
+            for r in problem_data.data.I
+        }
+        problem_data.resources.update_select(z)
+
+        s = {
+            (i, t): t == problem_data.data.min_t if z[i] else False
+            for i in problem_data.data.I for t in problem_data.data.T
+         }
+
+        u = {
+            (i, t): True
+            if t <= con_p + problem_data.data.TRP[i] and z[i] else False
+            for i in problem_data.data.I for t in problem_data.data.T
+        }
+
+        e = {
+            (i, t): True
+            if t == con_p + problem_data.data.TRP[i] and z[i] else False
+            for i in problem_data.data.I for t in problem_data.data.T
+        }
+
+        w = {
+            k: info['work'][k] * v == 1 for k, v in u.items()
+        }
+
+        r = {
+            k: info['rest'][k] * v == 1 for k, v in u.items()
+        }
+
+        er = {
+            (i, t): True
+            if (r[i, t] is True) and (r[i, t + 1] is not True)
+            else False
+            for i in problem_data.data.I
+            for t in problem_data.data.T_int(p_max=problem_data.data.max_t - 1)
+        }
+
+        er.update({
+            (i, problem_data.data.max_t): True
+            if r[i, problem_data.data.max_t] is True else False
+            for i in problem_data.data.I
+        })
+
+        tr = {
+            k: info['travel'][k] * v == 1 for k, v in u.items()
+        }
+
+        tr.update({
+            (i, t): True
+            for i in problem_data.data.I for t in problem_data.data.T
+            if u[(i, t)] is True and t > con_p
+        })
+
+        r.update({
+            (i, t): False
+            for i in problem_data.data.I for t in problem_data.data.T
+            if u[(i, t)] is True and t > con_p
+        })
+
+        w.update({
+            (i, t): False
+            for i in problem_data.data.I for t in problem_data.data.T
+            if u[(i, t)] is True and t > con_p
+        })
+
+        problem_data.resources_wildfire.update(
+            {(i, t): {
+                'start': s[i, t],
+                'use': u[i, t],
+                'end': e[i, t],
+                'work': w[i, t],
+                'travel': tr[i, t],
+                'rest': r[i, t],
+                'end_rest': er[i, t]
+            }
+                for i in problem_data.data.I for t in problem_data.data.T}
+        )
+
+        problem_data.groups_wildfire.update({
+            gt.get_index():
+                {'num_left_resources': max(
+                    0,
+                    gt.min_res_groups -
+                    sum([w[i, gt.get_index()[1]]
+                         for i in gt.group.resources.get_names()])
+                )}
+                if gt.get_index()[1] <= con_p else
+                {'num_left_resources': 0}
+            for gt in problem_data.groups_wildfire
+        })
+
+        problem_data.wildfire.update(
+            {t: {'contained': t >= con_p}
+             for t in problem_data.data.T})
+
+        return True
+    else:
+        return False
 # --------------------------------------------------------------------------- #
