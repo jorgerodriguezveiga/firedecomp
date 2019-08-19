859f21ef3c22bac79f49242a0bc48663909b46a3
Jorge <jorge.rodriguez.veiga@usc.es>
2019-08-19 12:17:54 +0200

    Add Final simulations

---
+++ simulations/20190517_CESGA/m-fix_work_nb-5_na-5_nm-20_np-20/execute.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -p thinnodes
+#SBATCH -t 20:00:00
+
+firedecomp_simulations -n 100 -nb 5 -na 5 -nm 20 -np 20 -m fix_work -so ../solver_options.yaml -vvv &> simulations.log
---
+++ simulations/Finales/m-original_nb-20_na-20_nm-20_np-60/execute.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -p thinnodes
+#SBATCH -t 20:00:00
+
+firedecomp_simulations -n 100 -nb 20 -na 20 -nm 20 -np 60 -m original -so ../solver_options.yaml -vvv &> simulations.log
