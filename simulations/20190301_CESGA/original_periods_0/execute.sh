3485b80006631bc0f1d166aaa341c6414676d69f
Jorge Rodriguez-Veiga <jorge.rodriguez.veiga@gmail.com>
2019-12-25 12:26:31 +0100

    Clean up simulation folder

--- simulations/20190301_CESGA/original_periods_0/execute.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#SBATCH -N 1
-#SBATCH -n 1
-#SBATCH -p thinnodes
-#SBATCH -t 33:00:00
-
-firedecomp_simulations -n 25 -nb 5 20 -na 5 20 -nm 5 20 -np 20 -m original -so ../solver_options.yaml -vvv &> simulations.log
--- simulations/20190603_CESGA/m-benders_scip_nb-20_na-5_nm-20_np-10/execute.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#SBATCH -N 1
-#SBATCH -n 1
-#SBATCH -p thinnodes
-#SBATCH -t 20:00:00
-
-firedecomp_simulations -n 100 -nb 20 -na 5 -nm 20 -np 10 -m benders_scip -so ../solver_options.yaml -vvv &> simulations.log
