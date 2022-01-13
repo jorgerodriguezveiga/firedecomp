6d40995f333264e72da75b75428c98368dfcddee
Jorge <jorge.rodriguez.veiga@usc.es>
2022-01-13 19:11:22 +0100

    Update simulation figures

--- firedecomp/plot/simulations.py
+++ firedecomp/plot/simulations.py
@@ -8,28 +8,27 @@ import operator
 def get_qgrid_options():
     return {
         # SlickGrid options
-        'fullWidthRows': True,
-        'syncColumnCellResize': True,
-        'forceFitColumns': False,
-        'defaultColumnWidth': 100,
-        'rowHeight': 28,
-        'enableColumnReorder': True,
-        'enableTextSelectionOnCells': True,
-        'editable': True,
-        'autoEdit': False,
-        'explicitInitialization': True,
-
+        "fullWidthRows": True,
+        "syncColumnCellResize": True,
+        "forceFitColumns": False,
+        "defaultColumnWidth": 100,
+        "rowHeight": 28,
+        "enableColumnReorder": True,
+        "enableTextSelectionOnCells": True,
+        "editable": True,
+        "autoEdit": False,
+        "explicitInitialization": True,
         # Qgrid options
-        'maxVisibleRows': 10,
-        'minVisibleRows': 10,
-        'sortable': True,
-        'filterable': True,
-        'highlightSelectedCell': False,
-        'highlightSelectedRow': True
+        "maxVisibleRows": 10,
+        "minVisibleRows": 10,
+        "sortable": True,
+        "filterable": True,
+        "highlightSelectedCell": False,
+        "highlightSelectedRow": True,
     }
 
 
-def read_csvs(folder='Finales', file='solution.csv'):
+def read_csvs(folder="Finales", file="solution.csv"):
     subfolders = os.listdir(folder)
     list_df = []
 
@@ -39,65 +38,99 @@ def read_csvs(folder='Finales', file='solution.csv'):
             file_path = os.path.join(file_dir, file)
             try:
                 list_df.append(
-                    pd.read_csv(file_path, sep=';', decimal=",", na_values="None"))
+                    pd.read_csv(
+                        file_path, sep=";", decimal=",", na_values="None"
+                    )
+                )
             except FileNotFoundError as e:
                 print(e)
 
     df = pd.concat(list_df, ignore_index=True)
-    df['cost'] = df["res_cost"] + df['wildfire_cost']
-    df['num_resources'] = df['num_brigades'] + df['num_aircraft'] + df[
-        'num_machines']
+    df["cost"] = df["res_cost"] + df["wildfire_cost"]
+    df["num_resources"] = (
+        df["num_brigades"] + df["num_aircraft"] + df["num_machines"]
+    )
 
     return df
 
 
 def get_best(
-    df, columns=None, groupby=None, filter_best=None, best_prefix='best_',
-    rel_prefix='rel_'):
+    df,
+    columns=None,
+    groupby=None,
+    filter_best=None,
+    best_prefix="best_",
+    rel_prefix="rel_",
+):
     if groupby is None:
-        groupby = ['num_brigades', 'num_aircraft', 'num_machines',
-                   'num_periods', 'seed']
+        groupby = [
+            "num_brigades",
+            "num_aircraft",
+            "num_machines",
+            "num_periods",
+            "seed",
+        ]
 
     if columns is None:
-        columns = [{'name': 'obj_fun', 'sense': 'min'},
-                   {'name': 'elapsed_time', 'sense': 'min'},
-                   {'name': 'solve_time', 'sense': 'min'}]
+        columns = [
+            {"name": "obj_fun", "sense": "min"},
+            {"name": "elapsed_time", "sense": "min"},
+            {"name": "solve_time", "sense": "min"},
+        ]
 
     if filter_best is None:
         filter_best = []
 
     filter_df = df.copy()
     for condition in filter_best:
-        op = getattr(operator, condition['operator'])
+        op = getattr(operator, condition["operator"])
         filter_df = filter_df[
-            op(filter_df[condition['column']], condition['value'])]
+            op(filter_df[condition["column"]], condition["value"])
+        ]
 
     sense_instances = pd.DataFrame()
     for column in columns:
-        sense_instances[best_prefix + column['name']] = getattr(
-            filter_df.groupby(groupby)[column['name']], column['sense'])()
+        sense_instances[best_prefix + column["name"]] = getattr(
+            filter_df.groupby(groupby)[column["name"]], column["sense"]
+        )()
 
     sense_instances = sense_instances.reset_index()
     sense_instances = sense_instances.rename(
-        columns={column['name']: best_prefix + column['name'] for column in
-                 columns})
+        columns={
+            column["name"]: best_prefix + column["name"] for column in columns
+        }
+    )
 
     new_df = pd.merge(filter_df, sense_instances, on=groupby)
 
     for column in columns:
-        best = new_df[best_prefix + column['name']]
-        new_df[rel_prefix + column['name']] = new_df[column['name']] / best
+        best = new_df[best_prefix + column["name"]]
+        new_df[rel_prefix + column["name"]] = new_df[column["name"]] / best
 
     return new_df
 
 
 def performance_profile_graph(
-        df, scatter_by='mode', x='elapsed_time', rename=None, conditions=None,
-        groupby=None, lines=None, columns=None, filter_best=None,
-        best_prefix='best_', rel_prefix='rel_', npoints=500, max_x=None,
-        xaxis=None, yaxis=None, image_filename=None, image_height=None,
-        image_width=None, layout=None
-        ):
+    df,
+    scatter_by="mode",
+    x="elapsed_time",
+    rename=None,
+    conditions=None,
+    groupby=None,
+    lines=None,
+    columns=None,
+    filter_best=None,
+    best_prefix="best_",
+    rel_prefix="rel_",
+    npoints=500,
+    max_x=None,
+    xaxis=None,
+    yaxis=None,
+    image_filename=None,
+    image_height=None,
+    image_width=None,
+    layout=None,
+):
     """
     Args:
         df: DataFrame.
@@ -119,13 +152,20 @@ def performance_profile_graph(
         max_x: maximum value in x axis. If None maximum value is considered.
     """
     if groupby is None:
-        groupby = ['num_brigades', 'num_aircraft', 'num_machines',
-                   'num_periods', 'seed']
+        groupby = [
+            "num_brigades",
+            "num_aircraft",
+            "num_machines",
+            "num_periods",
+            "seed",
+        ]
 
     if columns is None:
-        columns = [{'name': 'obj_fun', 'sense': 'min'},
-                   {'name': 'solve_time', 'sense': 'min'},
-                   {'name': 'elapsed_time', 'sense': 'min'}]
+        columns = [
+            {"name": "obj_fun", "sense": "min"},
+            {"name": "solve_time", "sense": "min"},
+            {"name": "elapsed_time", "sense": "min"},
+        ]
 
     if conditions is None:
         conditions = []
@@ -136,27 +176,31 @@ def performance_profile_graph(
     if rename is None:
         rename = {}
 
-    lines.update({
-        k: {} for k in set(df[scatter_by]).difference(set(lines.keys()))
-    })
+    lines.update(
+        {k: {} for k in set(df[scatter_by]).difference(set(lines.keys()))}
+    )
 
     if max_x is None:
         max_x = float("inf")
 
     df = get_best(
-        df, groupby=groupby, columns=columns, filter_best=filter_best,
-        best_prefix=best_prefix, rel_prefix=rel_prefix)
+        df,
+        groupby=groupby,
+        columns=columns,
+        filter_best=filter_best,
+        best_prefix=best_prefix,
+        rel_prefix=rel_prefix,
+    )
 
     num_instances = max(
-        [sum(df['mode'] == m) for m in set(df['mode'])]
+        [sum(df[scatter_by] == m) for m in set(df[scatter_by])]
     )
     performance = dict()
 
-    min_x = df[x].min()*0.999
-    max_x = min(df[x].max()*1.001, max_x)
-    step = (max_x - min_x)/npoints
-    x_range = [min_x + i * step for i in
-               range(npoints)]
+    min_x = df[x].min()
+    max_x = min(df[x].max(), max_x)
+    step = (max_x - 1) / npoints
+    x_range = [1 + i * step for i in range(npoints + 1)]
 
     for m in set(df[scatter_by]):
         performance[m] = dict()
@@ -164,9 +208,9 @@ def performance_profile_graph(
             m_df = df[df[scatter_by] == m]
             m_df = m_df[m_df[x] <= t]
             for condition in conditions:
-                op = getattr(operator, condition['operator'])
-                m_df = m_df[op(m_df[condition['column']], condition['value'])]
-            performance[m][t] = m_df.shape[0]/num_instances
+                op = getattr(operator, condition["operator"])
+                m_df = m_df[op(m_df[condition["column"]], condition["value"])]
+            performance[m][t] = m_df.shape[0] / num_instances
 
     performance_df = pd.DataFrame(performance)
 
@@ -178,45 +222,34 @@ def performance_profile_graph(
                 name = rename[c]
             fig.add_trace(
                 go.Scatter(
-                    x=performance_df.index,
-                    y=performance_df[c],
+                    x=[1] + list(performance_df.index),
+                    y=[0] + list(performance_df[c]),
                     name=name,
-                    line=lines[c]
+                    line=lines[c],
                 )
             )
-
-    fig.add_trace(
-        go.Scatter(
-            x=[min_x - (max_x-min_x)*0.01],
-            y=[0],
-            mode ='none',
-            showlegend=False
-        )
-    )
 
     if layout is None:
         layout = {}
 
-    layout.update(dict(
-        template='none',
-        xaxis=xaxis,
-        yaxis=yaxis
-    ))
+    layout.update(dict(template="none", xaxis=xaxis, yaxis=yaxis))
 
     fig.update_layout(layout)
     fig.show()
     if image_filename:
-        fig.write_image(
-            image_filename,
-            height=image_height,
-            width=image_width
-        )
+        fig.write_image(image_filename, height=image_height, width=image_width)
 
 
 def instance_comparison(
-        df, scatter_by='mode', reference='original', x='elapsed_time',
-        groupby=None, columns=None, best_prefix='best_',
-        rel_prefix='rel_'):
+    df,
+    scatter_by="mode",
+    reference="original",
+    x="elapsed_time",
+    groupby=None,
+    columns=None,
+    best_prefix="best_",
+    rel_prefix="rel_",
+):
     """
     Args:
         df: DataFrame.
@@ -228,16 +261,28 @@ def instance_comparison(
         rel_prefix: relative prefix.
     """
     if groupby is None:
-        groupby = ['num_brigades', 'num_aircraft', 'num_machines',
-                   'num_periods', 'seed']
+        groupby = [
+            "num_brigades",
+            "num_aircraft",
+            "num_machines",
+            "num_periods",
+            "seed",
+        ]
 
     if columns is None:
-        columns = [{'name': 'obj_fun', 'sense': 'min'},
-                   {'name': 'solve_time', 'sense': 'min'},
-                   {'name': 'elapsed_time', 'sense': 'min'}]
+        columns = [
+            {"name": "obj_fun", "sense": "min"},
+            {"name": "solve_time", "sense": "min"},
+            {"name": "elapsed_time", "sense": "min"},
+        ]
 
-    df = get_best(df, groupby=groupby, columns=columns,
-                  best_prefix=best_prefix, rel_prefix=rel_prefix)
+    df = get_best(
+        df,
+        groupby=groupby,
+        columns=columns,
+        best_prefix=best_prefix,
+        rel_prefix=rel_prefix,
+    )
 
     num_instances = sum(df[scatter_by] == list(df[scatter_by])[0])
     performance = dict()
@@ -248,36 +293,50 @@ def instance_comparison(
         # print("Scatter: {}".format(m))
         m_df = df[df[scatter_by] == m]
         performance[m] = (
-            ref_df[x].reset_index() - m_df[x].reset_index()).to_dict()[x]
+            ref_df[x].reset_index() - m_df[x].reset_index()
+        ).to_dict()[x]
 
     data = []
 
     for c in performance.keys():
         data.append(
-            go.Scatter(x=list(performance[c].keys()),
-                       y=list(performance[c].values()),
-                       name=c))
-
-    fig = dict(
-        data=data,
-        layout=dict(
-            template='none'
+            go.Scatter(
+                x=list(performance[c].keys()),
+                y=list(performance[c].values()),
+                name=c,
+            )
         )
-    )
+
+    fig = dict(data=data, layout=dict(template="none"))
 
     plotly.offline.iplot(fig)
 
 
 def sim_boxplot(
-    df, y='solve_time', modes=None, rename=None, columns=None, colors=None,
-    layout=None, xaxis=None, yaxis=None,
-    image_filename=None, image_width=None, image_height=None):
+    df,
+    y="solve_time",
+    modes=None,
+    rename=None,
+    columns=None,
+    colors=None,
+    layout=None,
+    xaxis=None,
+    yaxis=None,
+    image_filename=None,
+    image_width=None,
+    image_height=None,
+    sep=", ",
+):
     """Boxplot."""
     if modes is None:
-        modes = ['original', 'fix_work']
+        modes = ["original", "fix_work"]
     if columns is None:
-        columns = ['num_brigades', 'num_aircraft', 'num_machines',
-                   'num_periods']
+        columns = [
+            "num_brigades",
+            "num_aircraft",
+            "num_machines",
+            "num_periods",
+        ]
     if colors is None:
         colors = {}
     if layout is None:
@@ -286,22 +345,22 @@ def sim_boxplot(
     if rename is None:
         rename = {}
 
-    default_layout = {
-        'template': 'none'
-    }
+    default_layout = {"template": "none"}
 
     layout = layout.copy()
     layout.update(default_layout)
 
     if xaxis is not None:
-        layout.update({'xaxis': xaxis})
+        layout.update({"xaxis": xaxis})
     if yaxis is not None:
-        layout.update({'yaxis': yaxis})
+        layout.update({"yaxis": yaxis})
 
     fig = go.Figure()
-    for n, m in enumerate(modes):
-        x = ["-".join([str(j) for j in df.loc[[i], columns].values[0]])
-             for i in df[df['mode'] == m].index.tolist()]
+    for m in modes:
+        x = [
+            sep.join([str(j) for j in df.loc[[i], columns].values[0]])
+            for i in df[df["mode"] == m].index.tolist()
+        ]
 
         name = m
         if name in rename:
@@ -311,25 +370,18 @@ def sim_boxplot(
             fig.add_trace(
                 go.Box(
                     x=x,
-                    y=df[y][df['mode'] == m],
-                    legendgroup=m, name=name,
-                    marker_color=colors[m]['color']
+                    y=df[y][df["mode"] == m],
+                    legendgroup=m,
+                    name=name,
+                    marker_color=colors[m]["color"],
                 )
             )
         else:
             fig.add_trace(
-                go.Box(
-                    x=x,
-                    y=df[y][df['mode'] == m],
-                    legendgroup=m, name=name
-                )
+                go.Box(x=x, y=df[y][df["mode"] == m], legendgroup=m, name=name)
             )
 
     fig.update_layout(layout)
     fig.show()
     if image_filename:
-        fig.write_image(
-            image_filename,
-            height=image_height,
-            width=image_width
-        )
+        fig.write_image(image_filename, height=image_height, width=image_width)
